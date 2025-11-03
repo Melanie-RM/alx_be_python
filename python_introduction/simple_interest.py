8cd51c3d99a1377f9e8bf07c14d5f2d56aef1f2a
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-03 20:19:19 +0200

    added simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# simple interest calculation 
+p = 1000  # principal amount
+r = 5     # annual interest rate in percent
+t = 3     # time in years
+
+I = (p * r * t) / 100  # simple interest formula
+print("The simple interest is:", I)
