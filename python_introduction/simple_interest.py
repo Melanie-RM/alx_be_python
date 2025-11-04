924ef9cd16307fc060f9369bf2414091e1a8c747
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-04 06:42:13 +0200

    added formular

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@
 principal = 1000  # principal amount
 rate = 0.05     # annual interest rate in percent
 time = 3     # time in years
-
+i = (p*r*t)
 I = (principal * rate * time)   # simple interest formula
 print("The simple interest is:", I)
