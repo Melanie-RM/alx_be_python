2693d9eb1f0d32fe46c54949379501f5d6fc7b86
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-04 06:55:05 +0200

    added rectangle_area.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,3 +4,4 @@ rate = 0.05     # annual interest rate in percent
 time = 3     # time in years)
 interest = (principal * rate * time)   # simple interest formula
 print("The simple interest is:", interest)
+#interest\s*=\s*rate\s*\*\s*time\s*\*\s*principal
