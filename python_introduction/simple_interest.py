4e30db483345cc26b4bdb2587fed8fddc716437c
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-04 06:47:00 +0200

    fixed the formular for the 2nd time

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,6 @@
 # simple interest calculation 
 principal = 1000  # principal amount
 rate = 0.05     # annual interest rate in percent
-time = 3     # time in years
-i = (p*r*t)
-I = (principal * rate * time)   # simple interest formula
-print("The simple interest is:", I)
+time = 3     # time in years)
+interest = (principal * rate * time)   # simple interest formula
+print("The simple interest is:", interest)
