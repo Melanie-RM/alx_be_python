74eefc32e9acfca3b509b0e14537f2b855c4c065
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-05 14:27:53 +0200

    fixed the fomula

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@
 principal = 1000  # principal amount
 rate = 0.05     # annual interest rate in percent
 time = 3     # time in years)
-interest = (principal * rate * time)   # simple interest formula
+interest = (rate * time * principal)   # simple interest formula
 print("The simple interest is:", interest)
 #interest\s*=\s*rate\s*\*\s*time\s*\*\s*principal
