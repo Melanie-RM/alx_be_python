09e3e6687b205f8346d2992147fccc348468990b
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-15 12:04:17 +0200

    added multiplication_table.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,5 @@
+#calculation matching case
+num1 = int(input("Enter the first number:"))
+num2 = int(input("enter the second number:"))
+operation = input("Enter the operation (+, -, *, /): ")
+
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,5 @@
+#generate a multiplication table for a number
+number = int( input("Enter a number to see its multiplication table:"))
+
+for i in range(1,10):
+    print(number, "x", i ,"=" ,number * i)
