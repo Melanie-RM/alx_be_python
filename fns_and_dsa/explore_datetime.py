8457c96ed909600b0a367b42283bb55009191e66
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-11-24 12:57:06 +0200

    fixed the datetime.py file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,19 @@
 #datetime module to obtain today's date
-import datetime as dt
+from datetime import datetime, timedelta
 
-current_date = dt.date.today()
-print(current_date)
+current_date = datetime.today()
+#print(current_date)
+
+def display_current_datetime():
+    current_date = datetime.now().time()
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
+    
+display_current_datetime()
+
+def calculate_future_date():
+    days= int(input("enter number of days"))
+    future_date = current_date+timedelta(days)
+    print (future_date.strftime("%Y-%m-%d"))
+    
+calculate_future_date()
 
