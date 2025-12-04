ea731349d9b8d5bd6a0dc3262ad0303083be110a
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-12-04 13:23:49 +0200

    added additional test funtions

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,28 +6,28 @@ class TestSimpleCalculator(unittest.Test.Case):
     self.calc = SimpleCalculator()
 
 #addition tests
-    def test_addition_positive_numbers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(5,3),8)
     
     def test_addition_negative_numbers(self):
          self.assertEqual(self.calc.add(-2,-4),-6)
 
     #subtraction tests
-    def test_subtract_positive_numbers(self):
+    def test_subtract(self):
          self.assertEqual(self.calc.subtract(10,4),6)
     
     def test_subtract_negative_numbers(self):
           self.assertEqual(self.calc.subtract(-3,-7),-4)
     
     #multiplication tests
-    def test_multiply_numbers(self):
+    def test_multiply(self):
           self.assertEqual(self.calc.multiply(4,5),20)
     
     def test_multiply_by_zero(self):
           self.assertEqual(self.calc.multiply(10,0),0)
     
     #division tests
-    def test_divide_numbers(self):
+    def test_divide(self):
           self.assertEqual(self.calc.devide(20,5),4)
     
     def test_devide_floats(self):
