5e988ca15479865c5a63ec050ec035597ba91719
Ramoraswi Mathabatha <melaniermathabatha@gmail.com>
2025-12-04 11:36:48 +0200

    added the new folder

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,32 +8,32 @@ def perform_operations():
     if operation == "devide" :
         if num1>= num2:
             answer = num1 / num2
-            #print(answer)
+           
         elif num1 > 0 or num2 > 0:
             answer = num2 / num1
             
         elif num1 == 0 or num2 == 0:
             answer = "Cannot devide by zero"
-        #print(answer)
+       
         else:
             answer = num2/num1
-        #print(answer)
+        
 
     elif operation == "add":
         answer = num1 + num2
-        #print(answer)
+        
     
     elif operation == "multiply" :
         answer = num1 * num2
-        #print(answer)
+        
 
     elif operation == "subtract":
         if num1 > num2:
             answer = num1 - num2
-            #print(answer)
+            
         else :
             answer = num2 - num1
-            #print(answer)
+           
     print( answer)
 perform_operations()
     
